372b6d325c82c79a619f27fb6da707fe33ed2f2d
laurensnol <laurensn@t-online.de>
2020-10-16 17:10:33 +0200

    Created ItemDetailViewModel

---
+++ SwiftUI MVVM Example/ViewModels/ItemDetailViewModel.swift
@@ -0,0 +1,61 @@
+//
+//  ItemDetailViewModel.swift
+//  SwiftUI MVVM Example
+//
+//  Created by Laurens Nolting on 16.10.20.
+//
+
+import Foundation
+import SwiftUI
+import CoreData
+
+class ItemDetailViewModel: ObservableObject {
+    private let context = PersistenceController.shared.container.viewContext
+    
+    @Published var name = ""
+    
+    @Binding private var collectionDetailViewModel: CollectionDetailViewModel?
+    private var item: Item?
+    
+    init(appendingTo viewModel: Binding<CollectionDetailViewModel?>) {
+        self._collectionDetailViewModel = viewModel
+    }
+    
+    init(with item: Item) {
+        self._collectionDetailViewModel = .constant(nil)
+        self.item = item
+        self.name = item.name
+    }
+    
+    func save() {
+        if let item = item {
+            let request = NSFetchRequest<Item>(entityName: "Item")
+            request.predicate = NSPredicate(format: "id = %@", item.id.uuidString)
+            
+            do {
+                let results = try self.context.fetch(request)
+                if let itemToUpdate = results.first {
+                    itemToUpdate.name = self.name
+                }
+                
+                try self.context.save()
+            } catch {
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+            
+        } else {
+            let item = Item(context: self.context)
+            item.id = UUID()
+            item.name = self.name
+            self.collectionDetailViewModel?.items.append(item)
+            
+            do {
+                try self.context.save()
+            } catch {
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+}
