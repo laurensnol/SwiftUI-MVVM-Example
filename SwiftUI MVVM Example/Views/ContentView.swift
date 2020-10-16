7cf4302d6b1475a1437b31357b2ddbb87af55f83
laurensnol <laurensn@t-online.de>
2020-10-16 17:36:59 +0200

    Updated ContentView with proper UI

--- SwiftUI MVVM Example/Views/ContentView.swift
+++ SwiftUI MVVM Example/Views/ContentView.swift
@@ -11,12 +11,41 @@ import CoreData
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
+    @FetchRequest(
+        entity: Collection.entity(),
+        sortDescriptors: [NSSortDescriptor(keyPath: \Collection.name, ascending: true)]
+    ) private var collections: FetchedResults<Collection>
+    
     var body: some View {
         NavigationView {
             Form {
-                
+                ForEach(collections) { collection in
+                    NavigationLink(destination: CollectionDetailView(with: collection)) {
+                        HStack {
+                            Text(collection.name)
+                            Spacer()
+                            Text("\(collection.items.count) Items")
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                }
+                .onDelete { indexSet in
+                    for index in indexSet {
+                        self.viewContext.delete(self.collections[index])
+                    }
+                    
+                    do {
+                        try viewContext.save()
+                    } catch {
+                        let nsError = error as NSError
+                        fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                    }
+                }
             }
             .navigationTitle("Collections")
+            .navigationBarItems(trailing: NavigationLink(destination: CollectionDetailView()) {
+                Image(systemName: "plus.circle")
+            })
         }
     }
 }
